7bdea00e33766e05e69b9fda4e3192ca67106a21
srujan-garnayak <srujangarnayak@gmail.com>
2025-05-27 23:56:20 +0530

    minor changes to DocumentList component and added loopdocument.svg

--- app/(routes)/workspace/_components/DocumentList.jsx
+++ app/(routes)/workspace/_components/DocumentList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-
+import Image from "next/image";
 
 function DocumentList({ documentList }) {
   return (
     <div>
       {documentList.map((doc, index) => (
-        <div>
-          <div>
-            <h2>{doc.documentName}</h2>
+        <div key={index}>
+          <div className="flex items-center gap-2 pl-5">
+            {!doc.emoji && <Image src={'/loopdocument.svg'} width={20} height={20} /> }
+            <h2 className="flex gap-2">{doc?.emoji}{doc.documentName}</h2>
           </div>
         </div>
       ))}
